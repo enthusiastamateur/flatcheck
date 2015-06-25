86a703e8b636f7ad5309675cb1bbeddbd16d2a69
simontam <simon.tamas.hu@gmail.com>
2015-06-25 22:48:10 +0200

    Updated windows loader to check for java

--- app/Windows/FlatCheck.cmd
+++ app/Windows/FlatCheck.cmd
@@ -3,6 +3,21 @@ echo --------------------------------------
 echo FlatChecker startup script for Windows
 echo Type Ctrl-C to quit
 echo --------------------------------------
+echo.
+echo Checking for Java...
+echo.
+call java -version
+IF ERRORLEVEL 1 GOTO nojava
+echo.
 echo Setting path...
-set PATH=%PATH%;%CD%1
+set PATH=%PATH%;%CD%
 call java -jar flatcheck.jar
+GOTO noerror
+:nojava
+echo.
+echo It seems that Java RTE is not installed on your computer.
+echo Please download and install it from https://java.com/en/download/
+echo Then run this script again
+echo.
+:noerror
+pause
