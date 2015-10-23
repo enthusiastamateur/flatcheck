4cd96f3fba651e5fabed1947eb40279d6f89d664
simontam <simon.tamas.hu@gmail.com>
2015-10-23 17:39:58 +0200

    The 1.1 release of flatcheck. Added the following features: - Improved exception handling and more verbose output to console - Improved next page button handling by adding support to select using XPATH - Added functionality to scroll down the pages (for dinamically loading pages) - Added two new counters to control the max number of new page button clicks and max scroll downs on a site

--- src/main/scala/FlatCheck.scala
+++ src/main/scala/FlatCheck.scala
@@ -9,6 +9,8 @@
 
 import org.apache.commons.mail._
 import org.fluentlenium.core.FluentAdapter
+import org.fluentlenium.core.domain.{FluentList, FluentWebElement}
+import org.openqa.selenium.{JavascriptExecutor, By, WebElement, WebDriver}
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.ie.InternetExplorerDriver
@@ -19,6 +21,7 @@ import java.io.File
 import org.ini4j.ConfigParser
 import scala.collection.JavaConversions._
 import java.lang.Thread
+import java.util.Scanner
 
 class DataFile(filename: String) {
 
@@ -50,8 +53,14 @@ class DataFile(filename: String) {
         if (!line.isEmpty)    // skip empty  lines
       )    // split the lines
       yield (line.split("\t").toList)
-      if (contents.isEmpty) List() else contents.tail     // The head is the fejlec
-    } else List()
+      if (contents.isEmpty) {
+        println("  No previously checked links found in file " + filename)
+        List()
+      } else contents.tail     // The head is the fejlec
+    } else {
+      println("  No checked links file found at " + filename )
+      List()
+    }
   }
 }
 
@@ -61,13 +70,30 @@ class DataFile(filename: String) {
 object FlatCheck extends App {
   // Initalize parser
   val options = new ConfigParser
-  options.read("flatcheck.ini")
-
+  val iniName = "flatcheck.ini"
+  try {
+    options.read(iniName)
+  } catch {
+    case e: Exception => {
+      val currDir = new File("a").getAbsolutePath().dropRight(1)
+      println("Couldn't read ini file: " + currDir + iniName + "\nExiting FlatCheck...")
+      System.exit(1)
+    }
+  }
   // read password
   val address = options.get("general","address")
-  val stdIn = System.console()
   println("Please enter password for email address " + address + " :")
-  val passwd = String.valueOf(stdIn.readPassword())
+  val stdIn = System.console()
+  val passwd : String = if (stdIn == null)
+  {
+    println("Warning! Flatcheck was not able to locate your console, so it will ask for your password using plan text -> your password will be visible when you type it!")
+    val scanner = new Scanner(System.in)
+    scanner.nextLine()
+  }
+  else
+  {
+    String.valueOf(stdIn.readPassword())
+  }
   println("Starting FlatCheck...")
 
   def sendMessage(to: List[String], subject: String, msg: String) = {
@@ -81,131 +107,184 @@ object FlatCheck extends App {
     to.foreach(address => email.addTo(address))
     email.setMsg(msg)
     email.send()
-    println("  Emails sent!")
+    println("  Email sent to: " + to + "!")
   }
 
   // Variable to store if an email has already been sent
   var multiplePageWarningSent =  scala.collection.mutable.Map[String,Int]().withDefaultValue(0)
   // Email setup
   val emailSubject : String = options.get("general","emailsubject")
-  if (emailSubject.isEmpty) println("Email subject is not recognised!")
+  if (emailSubject.isEmpty) println("Email subject is not recognised, so emails will be sent without a subject!")
   val addAddresses : List[String] = options.get("general","sendto").split(",").toList
-  if (addAddresses.isEmpty) println("Sendto email addresses are not recognised!")
-
+  if (addAddresses.isEmpty) {
+    println("Sendto email addresses are not recognised! Update the ini file and restart the application! Exiting...")
+    System.exit(1)
+  }
   // Initalize the main loop
   val maxIter = options.get("general","exitafter").toInt
   val waitTime = options.get("general","refreshtime").toDouble
 
+  // Load the boundary on next page clicks, because it can happen that we end up in an infinite loop
+  val maxPageClicks = options.get("general","maxpageclicks").toInt
+
   // Start main loop
   mainloop(maxIter)
 
-
-
-
-
-
   /*
-   *  The main loop
+   *  The main loop, implemented as a nested function
    */
   def mainloop(iter: Int) : Unit = {
-
-    // Initalize browser
-    val browser = options.get("general","browser").toLowerCase match
+    // -------------------------------------------------
+    // Initalizing main loop
+    val driver : WebDriver = options.get("general","browser").toLowerCase match
     {
-      case "ie" | "internetexplorer" | "explorer" => new FluentAdapter(new InternetExplorerDriver())
-      case "chrome" => new FluentAdapter(new ChromeDriver())
-      case _ => new FluentAdapter(new FirefoxDriver())
+      case "ie" | "internetexplorer" | "explorer" => new InternetExplorerDriver()
+      case "chrome" => new ChromeDriver()
+      case _ => new FirefoxDriver()
     }
 
-    def iterateThroughPages(site: String, checkedAlreadyLinks: List[String], linksAcc: List[String]) : List[String] = {
-      val oldFirstHit = browser.$(options.get(site, "querystring")).get(0).getAttribute("href")
-      val nextRef = browser.$(options.get(site, "nextpagestring"))    // this will be a list of hrefs if there are new pages
-      nextRef.size() match {
-        case 0 => return linksAcc    // This means there is no next button we can click on!
-        case 1 => nextRef.click()
-        case _ => nextRef.get(0).click()
-      }
-      val newFirstHit = browser.$(options.get(site, "querystring")).get(0).getAttribute("href") // this is the new one
-      if (oldFirstHit != newFirstHit) {
-        val foundHits: Int = browser.$(options.get(site, "querystring")).size()  // number of found hits on new page
-        // This means successfully progressing to a new page, so harvest the links
-        val newLinks = {
-          for {
-            i <- 0 until foundHits
-            link: String = browser.$(options.get(site, "querystring")).get(i).getAttribute("href").split('?').toList(0)
-            if (!checkedAlreadyLinks.contains(link))
-          } yield link
-        }.toList
-        // Now let's go to the next page
-        iterateThroughPages(site, checkedAlreadyLinks, linksAcc ::: newLinks)
-      } else {
-        linksAcc    // If clicking didn't get us to a new page, let's finish
-      }
-    }
+    // Create the fluent adapter for the webdriver
+    val browser = new FluentAdapter(driver)
+    // End of main loop initalization
+    // -------------------------------------------------
+
 
     /*
      *  Nested function for getting new urls and saving them
      */
     def getNewURLsFromSite(site: String): List[String] = {
-      if (site == "general") List() else        // The "general" tag does not correspond to a site
-      {
-        val filename = options.get(site,"datafile")
-        try {
-          browser.goTo(options.get(site, "baseurl"))
+      /*
+       * Nested function to iterate through all found pages
+       */
+      def iterateThroughPages(checkedAlreadyLinks : List[String], linksAcc: List[String], clickCount : Int) : List[String] = {
+        // First scroll down to the bottom of the page.
+        // Some pages load their contents dynamically, so scroll as much as needed
+        val jse = driver.asInstanceOf[JavascriptExecutor]
+        var lastHeight = 0.toLong
+        var currHeight = jse.executeScript("return document.body.scrollHeight").asInstanceOf[Long]
+        var count = 0
+        val maxScrolls = try {
+          options.get(site, "maxscrolls").toInt
+        } catch {
+          case e:Exception =>
+            println("  Could not read number of max scrolls for site " + site + ". Using default value of 0.")
+            0
+        }
+
+        while (lastHeight != currHeight && count < maxScrolls)
+        {
+          jse.executeScript("window.scrollTo(0, document.body.scrollHeight);")
+          println("  Scrolled down page to bottom!")
+          Thread.sleep(1000)
+          lastHeight = currHeight
+          currHeight = jse.executeScript("return document.body.scrollHeight").asInstanceOf[Long]
+          count = count + 1
+        }
+
+
+        // Run the CSS query sting to locate the links on the current page
+        val linksSelector = options.get(site, "querystring")
+        val foundLinks: FluentList[FluentWebElement] = try {
+          browser.$(linksSelector)
+        } catch {
+          case e: Exception =>
+            println("  Error locating links on site \"" + site + "\" using selector string: " + linksSelector)
+            return List()
+        }
+        // Get the number of links found
+        val foundLinksSize = foundLinks.size()
+        println("  Located " + foundLinksSize + " offer links on current page!")
+
+        // Get new links on the current page
+        val newLinksOnPage: List[String] = {
+          for {
+            i <- 0 until foundLinksSize
+            link: String = foundLinks.get(i).getAttribute("href").split('?').toList(0)
+            if !checkedAlreadyLinks.contains(link)
+          } yield link
+        }.toList
+
+        // Try to find the new page button
+        val nextPageButtonSelector = options.get(site, "nextpagestring")
+        val nextPageButtonOption: Option[WebElement] = try {
+          // First try locating using xpath
+          Some(driver.findElement(By.xpath(nextPageButtonSelector)))
         } catch {
           case e: Exception => {
-            println("Error while loading site: " + site + ". Skipping it in this iteration...")
+            // Try also using CSS selector
+            val buttonOpt: Option[WebElement] = try {
+              Some(driver.findElement(By.cssSelector(nextPageButtonSelector)))
+            } catch {
+              case e: Exception =>
+                println("  Next page button not found!")
+                None
+            }
+            buttonOpt
+          }
+        }
+
+        // If there is a new page button, click it and collect the links there as well. Otherwise, return what we have acquired so far
+        nextPageButtonOption match {
+          case Some(nextPageButton) => {
+            // Click on the new page button, and wait 5 seconds for everything to load, if we have not exhaused all the clicks
+            if (clickCount < maxPageClicks) {
+              nextPageButton.click()
+              println("  Next page button clicked!")
+              Thread.sleep(5000.toLong)
+              iterateThroughPages(checkedAlreadyLinks, linksAcc ++ newLinksOnPage, clickCount + 1)
+            } else {
+              linksAcc ++ newLinksOnPage
+            }
+          }
+          case None => linksAcc ++ newLinksOnPage
+        }
+      }
+
+      // -------------------------------------------------
+      // Body of getNewURLsFromSite
+      if (site == "general") List() else        // The "general" tag does not correspond to a site
+      {
+        println("  --------------------------------------")
+        println("  Site: " + site)
+        val filename = options.get(site,"datafile")
+        val baseUrl = options.get(site, "baseurl")
+        try {
+          browser.goTo(baseUrl)
+        } catch {
+          case e: Exception => {
+            println("  Error loading site using URL:\n  " + baseUrl + "\n  Please check manually if it works!" +
+              "\n  Skipping it in this iteration...")
             return List()
           }
         }
         val checkedAlready: List[List[String]] = new DataFile(filename).readFlats
         val checkedAlreadyLinks : List[String] = checkedAlready map {list => list(1)}
-        try {
-          // Check how many hits are on the current page and the totel number of hits reported by the site
-          val foundHits: Int = browser.$(options.get(site, "querystring")).size()
-          val reportedHits: Int = browser.$(options.get(site, "hitsstring")).getText.replaceAll("\\D+","").toInt
-          // Get new links on the first page
-          val firstNewLinks = {
-            for {
-              i <- 0 until foundHits
-              link: String = browser.$(options.get(site, "querystring")).get(i).getAttribute("href").split('?').toList(0)
-              if (!checkedAlreadyLinks.contains(link))
-            } yield link
-          }.toList
 
+        // Iterate through all pages
+        val newLinks = iterateThroughPages(checkedAlreadyLinks, List(), 0)
 
-          if (foundHits != reportedHits)    //this means we need to check the other pages
-          {
-            println("  Warning: Multiple pages found for site: " + site)
-          }
-
-          val newLinks = iterateThroughPages(site,checkedAlreadyLinks,firstNewLinks)
-
-          // Update the datafiles
-          if (!newLinks.isEmpty)
-          {
-            val dataFile = new DataFile(filename)
-            newLinks.foreach(link => dataFile.appendFlat(link,true))
-            println("  Found " + newLinks.size + " new offers on site: " + site)
-          }
-          else
-          {
-            println("  No new offers found for site: " + site)
-          }
-          newLinks
-
-        } catch {
-          case e: Exception => {
-            println("Error while querying site: " + site + ". Skipping it in this iteration...")
-            return List()
-          }
+        // Update the datafiles
+        if (!newLinks.isEmpty)
+        {
+          val dataFile = new DataFile(filename)
+          newLinks.foreach(link => dataFile.appendFlat(link,true))
+          println("  Found " + newLinks.size + " new offers!")
         }
+        else
+        {
+          println("  No new offers found!")
+        }
+        newLinks
+
       }
+      // End of body of getNewURLsFromSite
+      // -------------------------------------------------
     }
 
-    /*
-     * The settings are reread each iteration. This makes it possible to edit them on the fly!
-     */
+    // -------------------------------------------------
+    // Body of main loop
+    //
+    // The settings are reread on each iteration. This makes is possible to edit them on the fly!
     println("Beginning iteration #: " + iter)
     val sites: List[String] = options.sections().toList
 
@@ -213,6 +292,7 @@ object FlatCheck extends App {
     val allNewLinks = sites.flatMap(getNewURLsFromSite(_))
 
     // Send email if there are new offers
+    println("--------------------------------------")
     if (!allNewLinks.isEmpty)
     {
       println("  Found " + allNewLinks.size + " new offers alltogether!")
@@ -229,7 +309,6 @@ object FlatCheck extends App {
       mainloop(iter - 1)
     }
   }
-
-
-
+  // End of body of main loop
+  // -------------------------------------------------
 }
