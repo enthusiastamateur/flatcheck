c5632b95c98f70a72187ae2d231ac9cab8ba4058
Tamas Simon <simon.tamas.hu@gmail.com>
2015-11-15 17:14:51 +0100

    - Updated selenium and fluentlenium to a newer version. - Cleaned compiler warnings - Added ini file to repo for easier testing. - Added extra info for button clicking

--- build.sbt
+++ build.sbt
@@ -4,8 +4,8 @@ version := "1.0"
 
 libraryDependencies += "org.apache.commons" % "commons-email" % "1.3.3"
 
-libraryDependencies += "org.seleniumhq.selenium" % "selenium-java" % "2.44.0"
+libraryDependencies += "org.seleniumhq.selenium" % "selenium-java" % "2.48.2"
 
-libraryDependencies += "org.fluentlenium" % "fluentlenium-core" % "0.10.2"
+libraryDependencies += "org.fluentlenium" % "fluentlenium-core" % "0.10.8"
 
 libraryDependencies += "org.ini4j" % "ini4j" % "0.5.2"
--- src/main/scala/FlatCheck.scala
+++ src/main/scala/FlatCheck.scala
@@ -14,7 +14,6 @@ import org.openqa.selenium.{JavascriptExecutor, By, WebElement, WebDriver}
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.ie.InternetExplorerDriver
-import scala.io.Source
 import java.util.Calendar
 import java.io.FileWriter
 import java.io.File
@@ -38,7 +37,7 @@ class DataFile(filename: String) {
     }
     // Append to the end of file
     val appender = new FileWriter(filename,true)
-    appender.write("\n" + Calendar.getInstance().getTime().toString + "\t" + link + "\t" + emailSent.toString)
+    appender.write("\n" + Calendar.getInstance.getTime.toString + "\t" + link + "\t" + emailSent.toString)
     appender.flush()
     appender.close()
   }
@@ -50,9 +49,9 @@ class DataFile(filename: String) {
     {
       val contents = for (
         line <- io.Source.fromFile(filename).getLines.toList
-        if (!line.isEmpty)    // skip empty  lines
+        if line.isEmpty    // skip empty  lines
       )    // split the lines
-      yield (line.split("\t").toList)
+      yield line.split("\t").toList
       if (contents.isEmpty) {
         println("  No previously checked links found in file " + filename)
         List()
@@ -74,11 +73,10 @@ object FlatCheck extends App {
   try {
     options.read(iniName)
   } catch {
-    case e: Exception => {
-      val currDir = new File("a").getAbsolutePath().dropRight(1)
+    case e: Exception =>
+      val currDir = new File("a").getAbsolutePath.dropRight(1)
       println("Couldn't read ini file: " + currDir + iniName + "\nExiting FlatCheck...")
       System.exit(1)
-    }
   }
   // read password
   val address = options.get("general","address")
@@ -110,8 +108,6 @@ object FlatCheck extends App {
     println("  Email sent to: " + to + "!")
   }
 
-  // Variable to store if an email has already been sent
-  var multiplePageWarningSent =  scala.collection.mutable.Map[String,Int]().withDefaultValue(0)
   // Email setup
   val emailSubject : String = options.get("general","emailsubject")
   if (emailSubject.isEmpty) println("Email subject is not recognised, so emails will be sent without a subject!")
@@ -199,7 +195,7 @@ object FlatCheck extends App {
         val newLinksOnPage: List[String] = {
           for {
             i <- 0 until foundLinksSize
-            link: String = foundLinks.get(i).getAttribute("href").split('?').toList(0)
+            link: String = foundLinks.get(i).getAttribute("href").split('?').head
             if !checkedAlreadyLinks.contains(link)
           } yield link
         }.toList
@@ -210,7 +206,7 @@ object FlatCheck extends App {
           // First try locating using xpath
           Some(driver.findElement(By.xpath(nextPageButtonSelector)))
         } catch {
-          case e: Exception => {
+          case e: Exception =>
             // Try also using CSS selector
             val buttonOpt: Option[WebElement] = try {
               Some(driver.findElement(By.cssSelector(nextPageButtonSelector)))
@@ -220,22 +216,20 @@ object FlatCheck extends App {
                 None
             }
             buttonOpt
-          }
         }
 
         // If there is a new page button, click it and collect the links there as well. Otherwise, return what we have acquired so far
         nextPageButtonOption match {
-          case Some(nextPageButton) => {
+          case Some(nextPageButton) =>
             // Click on the new page button, and wait 5 seconds for everything to load, if we have not exhaused all the clicks
             if (clickCount < maxPageClicks) {
+              println("  Clicking on next page button with text '" + nextPageButton.getText + "'...")
               nextPageButton.click()
-              println("  Next page button clicked!")
               Thread.sleep(5000.toLong)
               iterateThroughPages(checkedAlreadyLinks, linksAcc ++ newLinksOnPage, clickCount + 1)
             } else {
               linksAcc ++ newLinksOnPage
             }
-          }
           case None => linksAcc ++ newLinksOnPage
         }
       }
@@ -251,11 +245,10 @@ object FlatCheck extends App {
         try {
           browser.goTo(baseUrl)
         } catch {
-          case e: Exception => {
+          case e: Exception =>
             println("  Error loading site using URL:\n  " + baseUrl + "\n  Please check manually if it works!" +
               "\n  Skipping it in this iteration...")
             return List()
-          }
         }
         val checkedAlready: List[List[String]] = new DataFile(filename).readFlats
         val checkedAlreadyLinks : List[String] = checkedAlready map {list => list(1)}
@@ -264,10 +257,10 @@ object FlatCheck extends App {
         val newLinks = iterateThroughPages(checkedAlreadyLinks, List(), 0)
 
         // Update the datafiles
-        if (!newLinks.isEmpty)
+        if (newLinks.nonEmpty)
         {
           val dataFile = new DataFile(filename)
-          newLinks.foreach(link => dataFile.appendFlat(link,true))
+          newLinks.foreach(link => dataFile.appendFlat(link,emailSent = true))
           println("  Found " + newLinks.size + " new offers!")
         }
         else
@@ -289,11 +282,11 @@ object FlatCheck extends App {
     val sites: List[String] = options.sections().toList
 
     // Get new links from all sites
-    val allNewLinks = sites.flatMap(getNewURLsFromSite(_))
+    val allNewLinks = sites.flatMap(getNewURLsFromSite)
 
     // Send email if there are new offers
     println("--------------------------------------")
-    if (!allNewLinks.isEmpty)
+    if (allNewLinks.nonEmpty)
     {
       println("  Found " + allNewLinks.size + " new offers alltogether!")
       sendMessage(addAddresses,emailSubject,options.get("general","emailfixcontent") + " \n" + allNewLinks.mkString("\n"))
