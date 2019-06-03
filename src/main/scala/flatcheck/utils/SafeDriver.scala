d5ec94e6e2328bb90d358f622e329ce91b7559fd
Simon Tamas <simon.tamas.hu@gmail.com>
2019-06-03 23:02:19 +0200

    Fixed chromedriver path

--- src/main/scala/flatcheck/utils/SafeDriver.scala
+++ src/main/scala/flatcheck/utils/SafeDriver.scala
@@ -24,8 +24,8 @@ class SafeDriver(val options: FlatcheckConfig, val logger: Logger) {
     case Unix => "/usr/bin/google-chrome"
   }
   private val chromeDriverLocation  = Utils.getOSType() match {
-    case Windows => "./chrome/chromedriver_74.0.3729.6.exe"
-    case Unix => "./chrome/chromedriver_74.0.3729.6_linux_64"
+    case Windows => "./chromedriver/chromedriver_74.0.3729.6.exe"
+    case Unix => "./chromedriver/chromedriver_74.0.3729.6_linux_64"
   }
   private var driver : WebDriver = createWebDriver()
   logger.debug(s"Prewarming driver")
